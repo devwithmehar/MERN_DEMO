cf6c8619605af4bfdde457460c0273e632a0f40c
Sahil <smehar@aceproject.space>
2021-05-06 16:41:11 -0500

    app is using server.js

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,34 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const client = require('./routers/client')
 
 //Assigning the port to the backend server
 const port = process.env.PORT || 5000;
 
-//it will let the application know that we are using express.
+//Creating a server
 const app = express();
 
 //It will let the application know that we are using json format
 app.use(express.json());
+
+const mongo = process.env.Mongo_CONNECTION;
+
+//connecting the application with our cluster
+mongoose.connect(mongo,{
+    useNewUrlParser:true,
+    useCreateIndex: true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+})
+
+const connection = mongoose.connection;
+
+connection.once('open',() =>{
+    console.log('The Database has been Established');
+})
+
+app.use('/client',client)
 
 app.listen(port,()=>{
     console.log(`The Server is running on PORT NO: ${port}`);
