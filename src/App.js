4899463a124e17cfc8519648b085d6c61cefad78
Sahil <smehar@aceproject.space>
2021-05-09 13:40:30 -0500

    components imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Registeration from './components/Registeration'
+import './Style/App.css'
+import Home from './components/Home'
+import {BrowserRouter as Router, Switch , Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-     <h1>Begining of E-Commerce website</h1>
+    <div id="main">
+
+      <Router>
+          <div id="App">
+           <Navbar />
+            <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Registeration} />
+            </Switch>
+         </div>
+        </Router>
     </div>
   );
 }
