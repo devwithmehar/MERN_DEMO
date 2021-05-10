4df8f02ae1aec326fd3b1f2cd52d98a722e6c54f
Sahil <smehar@aceproject.space>
2021-05-10 14:24:31 -0500

    Clinet Model is changed

--- backend/models/client.model.js
+++ backend/models/client.model.js
@@ -13,13 +13,9 @@ const clientSchema = new mongoose.Schema({
         lowercase: true,
         trim: true
     },
-    firstName:{
+    fullName:{
         type:String,
-        required:[true,"First Name is required"],
-        trim: true
-    },
-    lastName:{
-        type:String,
+        required:[true," Name is required"],
         trim: true
     },
     phone:{
