4ae9ca9206032d67e08dcb503c2d5765989926b3
Sahil <smehar@aceproject.space>
2021-05-15 15:02:06 -0500

    components imported

--- src/App.js
+++ src/App.js
@@ -4,17 +4,46 @@ import Registeration from './components/Registeration'
 import './Style/App.css'
 import Home from './components/Home'
 import {BrowserRouter as Router, Switch , Route} from 'react-router-dom'
+import {React , useEffect , useState} from 'react';
+
+
 
 
 function App() {
+  
+
+  const [login, setLogin] = useState(false)
+  const [fullName, setFullName] = useState('')
+  const [username, setUsername] = useState('')
+ 
+  useEffect(() => {   
+    console.log('I am practicing UseEffect')
+    
+    if(  localStorage.getItem('token'))
+    {
+      
+       setLogin(prev => !prev);
+       setFullName(localStorage.getItem('FullName'));
+       setUsername(localStorage.getItem('Username'));
+       
+    }
+    
+ }, []);
+
+
+
   return (
     <div id="main">
 
       <Router>
           <div id="App">
-           <Navbar />
+           
+      
             <Switch>
-            <Route path='/' exact component={Home} />
+            <Route exact path='/'  >
+              <Navbar  name={fullName} isLogin={login} username={username} />
+              <Home />
+            </Route>
             
             <Route path='/login' component={Login} />
            
